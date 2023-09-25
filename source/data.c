a3f16cbd37ae5862cc8e7dd96fb7af0de77ffb8c
AlexPinto14 <apinto141414@gmail.com>
2023-09-25 16:11:47 +0100

    Implemented data_replace

--- source/data.c
+++ source/data.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <data.h>
+#include <string.h>
 
 
 struct data_t *data_create(int size, void *data) {
@@ -41,9 +42,30 @@ struct data_t *data_dup(struct data_t *data) {
         free(new_data);
         return NULL;
     }
-
     // If everything goes right, copies the data to the new data structure
     memcpy(new_data->data, data->data, data->datasize);
 
     return new_data;
+}
+
+
+int data_replace(struct data_t *data, int new_size, void *new_data) {
+    if (data == NULL || new_data == NULL) {
+        return -1;
+    }
+    // Frees the memory of the old data
+    free(data->data);
+    // Updates the new size of the data
+    data->datasize = new_size;
+    // Allocs the memory for the new data
+    data->data = malloc(new_size);
+
+    if (data->data == NULL) {
+        return -1;
+    }
+
+    // If the data is valid, copies the new data to the data structure
+    memcpy(data->data, new_data, new_size);
+
+    return 0;
 }
