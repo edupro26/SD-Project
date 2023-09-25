72ab58521afe259822aef5776add29bcc51e1d9b
AlexPinto14 <apinto141414@gmail.com>
2023-09-25 15:47:58 +0100

    Implemented data_duplicate

--- source/data.c
+++ source/data.c
@@ -21,3 +21,29 @@ int data_destroy(struct data_t *data) {
     free(data);
     return 0;
 }
+
+
+struct data_t *data_dup(struct data_t *data) {
+    if (data == NULL || data->data == NULL) {
+        return NULL;
+    }
+
+    struct data_t *new_data = (struct data_t *) malloc(sizeof(struct data_t));
+    if (new_data == NULL) {
+        return NULL;
+    }
+
+    new_data->datasize = data->datasize;
+    // Allocs the memory of the new data structure
+    new_data->data = malloc(data->datasize);
+    if(new_data->data == NULL) {
+        // In case of error, frees the memory previously allocated
+        free(new_data);
+        return NULL;
+    }
+
+    // If everything goes right, copies the data to the new data structure
+    memcpy(new_data->data, data->data, data->datasize);
+
+    return new_data;
+}
