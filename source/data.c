aac2ded7c65ef3eb61935238d1a95d60f58190dd
AlexPinto14 <apinto141414@gmail.com>
2023-09-25 14:55:37 +0100

    Implemented data_destroy

--- source/data.c
+++ source/data.c
@@ -11,3 +11,13 @@ struct data_t *data_create(int size, void *data) {
     new_data->data = data;
     return new_data;
 }
+
+
+int data_destroy(struct data_t *data) {
+    if (data == NULL) {
+        return -1;
+    }
+    free(data->data);
+    free(data);
+    return 0;
+}
