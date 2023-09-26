f1a9c856cb011f7dbc80d1f9adef40e995c67172
tiagomiguel29 <66640308+tiagomiguel29@users.noreply.github.com>
2023-09-26 08:54:02 +0100

    entry_dup, entry_destroy made

--- source/entry.c
+++ source/entry.c
@@ -15,3 +15,34 @@ struct entry_t *entry_create(char *key, struct data_t *data) {
 
     return new_entry;
 }
+
+int entry_destroy(struct entry_t *entry) {
+    if (entry == NULL) {
+        return -1;
+    }
+    free(entry->key);
+    data_destroy(entry->value);
+    free(entry);
+    return 0;
+}
+
+/* Função que duplica uma entry, reservando a memória necessária para a
+ * nova estrutura.
+ * Retorna a nova entry ou NULL em caso de erro.
+ */
+struct entry_t *entry_dup(struct entry_t *entry) {
+    if (entry == NULL) {
+        return NULL;
+    }
+
+    struct entry_t *new_entry = malloc(sizeof(struct entry_t));
+    if (new_entry == NULL) {
+        return NULL;
+    }
+
+    new_entry->key = entry->key;
+    new_entry->value = data_dup(entry->value);
+
+    return new_entry;
+}
+
