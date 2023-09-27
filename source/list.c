5b8677f3654f8659be2e30ee3be69bc91e172daf
tiagomiguel29 <66640308+tiagomiguel29@users.noreply.github.com>
2023-09-28 00:31:33 +0100

    Finished list module. DupKey test failing on 211

--- source/list.c
+++ source/list.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "list-private.h"
 
 struct list_t *list_create() {
@@ -9,6 +11,7 @@ struct list_t *list_create() {
     }
     // List starts empty
     new_list->size = 0;
+    new_list->head = NULL;
 
     return new_list;
 }
@@ -23,7 +26,7 @@ int list_destroy(struct list_t *list) {
 
     while (current != NULL) {
         next = current->next;
-        free(current);
+        //free(current);
         current = next;
     }
 
@@ -76,7 +79,7 @@ int list_add(struct list_t *list, struct entry_t *entry) {
         if (compare == 0) {
             new_node->next = current->next;
             entry_destroy(current->entry);
-            free(current);
+            //free(current);
             current = new_node;
             return 1;
         }
@@ -91,3 +94,96 @@ int list_add(struct list_t *list, struct entry_t *entry) {
     list->size++;
     return 0;
 }
+
+int list_remove(struct list_t *list, char *key) {
+    if (list == NULL || key == NULL) {
+        return -1;
+    }
+
+    struct node_t *current = list->head;
+    struct node_t *previous = NULL;
+
+    while (current != NULL) {
+        if (strcmp(current->entry->key, key) == 0) {
+            // If previous is NULL, remove head
+            if (previous == NULL) {
+                list->head = current->next;
+            } else {
+                previous->next = current->next;
+            }
+            entry_destroy(current->entry);
+            free(current);
+            list->size--;
+            return 0;
+        }
+        previous = current;
+        current = current->next;
+    }
+
+    return 1;
+}
+
+
+struct entry_t *list_get(struct list_t *list, char *key) {
+    if (list == NULL || key == NULL) {
+        return NULL;
+    }
+
+    struct node_t *current = list->head;
+    
+    while (current != NULL) {
+        if (strcmp(current->entry->key, key) == 0) {
+            return current->entry;
+        }
+        current = current->next;
+    }
+
+    return NULL;
+}
+
+int list_size(struct list_t *list) {
+    if (list == NULL) {
+        return -1;
+    }
+
+    return list->size;
+}
+
+char **list_get_keys(struct list_t *list) {
+    if (list == NULL || list->size == 0) {
+        return NULL;
+    }
+
+    char **keys = malloc((list->size + 1) * sizeof(char *));
+    if (keys == NULL) {
+        return NULL;
+    }
+
+    struct node_t *current = list->head;
+    int i = 0;
+
+    while (current != NULL) {
+        keys[i] = strdup(current->entry->key);
+        current = current->next;
+        i++;
+    }
+
+    keys[i] = NULL;
+    return keys;
+}
+
+int list_free_keys(char **keys) {
+    if (keys == NULL) {
+        return -1;
+    }
+
+    int i = 0;
+    while (keys[i] != NULL) {
+        free(keys[i]);
+        i++;
+    }
+
+    free(keys);
+    return 0;
+}
+
