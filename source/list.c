cc7b8779b0d1d85ebe25f2436a36485c9d83dd0e
tiagomiguel29 <66640308+tiagomiguel29@users.noreply.github.com>
2023-09-27 22:29:14 +0100

    Implemented list_add

--- source/list.c
+++ source/list.c
@@ -30,3 +30,64 @@ int list_destroy(struct list_t *list) {
     free(list);
     return 0;
 }
+
+int list_add(struct list_t *list, struct entry_t *entry) {
+    if (list == NULL || entry == NULL) {
+        return -1;
+    }
+
+    struct node_t *new_node = malloc(sizeof(struct node_t));
+    if (new_node == NULL) {
+        return -1;
+    }
+
+    new_node->entry = entry;
+    new_node->next = NULL;
+
+    // If list is empty, add node as head
+    if (list->size == 0) {
+        list->head = new_node;
+        list->size++;
+        return 0;
+    }
+
+    // If list is not empty, find the right place to insert the node
+    struct node_t *current = list->head;
+    struct node_t *previous = NULL;
+
+    while (current != NULL) {
+        int compare = entry_compare(entry, current->entry);
+
+        // If entry is smaller than current, insert before current
+        if (compare < 0) {
+            // If previous is NULL, insert at head
+            if (previous == NULL) {
+                new_node->next = current;
+                list->head = new_node;
+            } else {
+                previous->next = new_node;
+                new_node->next = current;
+            }
+            list->size++;
+            return 0;
+        }
+
+        // If entry is equal to current, replace current
+        if (compare == 0) {
+            new_node->next = current->next;
+            entry_destroy(current->entry);
+            free(current);
+            current = new_node;
+            return 1;
+        }
+
+        // If entry is bigger than current, continue
+        previous = current;
+        current = current->next;
+    }
+
+    // If entry is bigger than all entries, insert at tail
+    previous->next = new_node;
+    list->size++;
+    return 0;
+}
