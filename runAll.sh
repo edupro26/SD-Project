c373f2ef671511fc30e0a379d2e544baf2022c0c
tiagomiguel29 <66640308+tiagomiguel29@users.noreply.github.com>
2023-10-10 15:47:21 +0100

    Removed previous scripts

--- runAll.sh
+++ runAll.sh
@@ -4,17 +4,3 @@
 # Run all test files
     echo "\nRunning tests..."
 
-    echo "\nTesting data:"
-    ./binary/test_data
-
-    echo "\nTesting entry:"
-    ./binary/test_entry
-
-    echo "\nTesting list:"
-    ./binary/test_list
-
-    echo "\nTesting table:"
-    ./binary/test_table
-
-    echo "\nTesting serialization:"
-    ./binary/test_serialization
