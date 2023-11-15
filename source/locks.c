4865c9b48b93a5fe7466d22e5600c03fbc5b88dc
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2023-11-15 20:12:16 +0000

    Started new implementation for locks

--- source/locks.c
+++ source/locks.c
@@ -38,3 +38,59 @@ void locks_lock_stats() {
 void locks_unlock_stats() {
     pthread_mutex_unlock(&mutex_stats);
 }
+
+
+typedef struct {
+    pthread_mutex_t mutex;
+    pthread_cond_t read, put;
+    int reads, puts, wputs;
+} locks_t;
+locks_t lock;
+
+void init_data_lock(locks_t *lock) {
+    pthread_mutex_init(&lock->mutex, NULL);
+    pthread_cond_init(&lock->read, NULL);
+    pthread_cond_init(&lock->put, NULL);
+    lock->reads = lock->puts = lock->wputs = 0;
+}
+
+void get(locks_t *lock) {
+    pthread_mutex_lock(&lock->mutex);
+    while (lock->puts > 0 || lock->wputs > 0) {
+        pthread_cond_wait(&lock->read, &lock->mutex);
+    }
+    lock->reads++;
+    pthread_mutex_unlock(&lock->mutex);
+}
+
+void leaveGet(locks_t *lock) {
+    pthread_mutex_lock(&lock->mutex);
+    lock->reads--;
+    if (lock->reads == 0 && lock->wputs > 0) {
+        pthread_cond_signal(&lock->put);
+    }
+    pthread_mutex_unlock(&lock->mutex);
+}
+
+void put(locks_t *lock) {
+    pthread_mutex_lock(&lock->mutex);
+    lock->wputs++;
+    while (lock->reads > 0 || lock->puts > 0) {
+        pthread_cond_wait(&lock->put, &lock->mutex);
+    }
+    lock->wputs--;
+    lock->puts++;
+    pthread_mutex_unlock(&lock->mutex);
+}
+
+void leavePut(locks_t *lock) {
+    pthread_mutex_lock(&lock->mutex);
+    lock->puts--;
+    if (lock->wputs > 0) {
+        pthread_cond_signal(&lock->put);
+    } else {
+        pthread_cond_broadcast(&lock->read);
+    }
+    pthread_mutex_unlock(&lock->mutex);
+}
+
