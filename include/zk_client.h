e81ee67ef22404cdbc1cf2361c413cf1f3e262ca
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2023-12-05 01:08:59 +0000

    Made comments for zk_client.h

--- include/zk_client.h
+++ include/zk_client.h
@@ -26,18 +26,25 @@ static char *root_path = "/chain";
 
 static char *watcher_ctx = "ZooKeeper Data Watcher";
 
+// Initialize the zookeeper connection and the connections to the nodes for read and write. Returns a struct with the names of the head and tail nodes and the connections to the nodes for read and write
 struct rtable_pair_t *zk_init(char *address_port);
 
+// Watcher function for children events. Calls the function to make the connections to the nodes for read and write when events occur
 void zk_children_handler(zhandle_t *zh, int type, int state, const char *path, void *watcherCtx);
 
+// Watcher function for the connection to zookeeper
 void zk_connection_watcher(zhandle_t *zh, int type, int state, const char *path, void *watcherCtx);
 
+// Given a list of nodes, returns the node with the lowest id
 char *zk_get_head(char **node_list, int size);
 
+// Given a list of nodes, returns the node with the highest id
 char *zk_get_tail(char **node_list, int size);
 
+// Returns the data of the node with the given name
 char **zk_get_data(char *node_name);
 
+// Make connections to the nodes for read and write. Returns 0 if the connections are successful, -1 otherwise
 int make_connections(char *head_name, char *tail_name, struct rtable_pair_t *rtable_pair);
 
 
